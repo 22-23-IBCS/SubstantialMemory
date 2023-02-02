6479a35ec70fffdab663f4842e78a079269a4bd2
Joseph <joseph_song@aw.org>
2023-02-02 13:12:43 -0800

    challenge problem

--- gravity.py
+++ gravity.py
@@ -4,14 +4,41 @@ def main():
     win = GraphWin("demo", 500, 500)
     ball=Circle(Point(250,250), 10)
     x=0
-    start=time.time()
     posy=ball.getCenter().getY()
     ball.draw(win)
-    while (posy-10)>0:
-        now=time.time()
-        x=x+9.8*(now-start)
-        ball.move(0,x)
-        posy=ball.getCenter().getY()
+    now=time.time()
+    while True:
+        while (posy+25)<500:
+            now1=now
+            now=time.time()
+            posy=ball.getCenter().getY()
+            ball.move(0,x)
+            x=x+9.8*(now-now1)
+            now=time.time()
+            print("1")
+        while x>0:
+            now1=now
+            now=time.time()
+            negx= x-2*x
+            ball.move(0, negx)
+            x=x-9.8*(now-now1)
+            posy=ball.getCenter().getY()
+            now=time.time()
+            print(x)
+            print(negx)
+        if posy>450:
+            break
+    while (posy+25)<500:
+            now1=now
+            now=time.time()
+            posy=ball.getCenter().getY()
+            ball.move(0,x)
+            x=x+9.8*(now-now1)
+            now=time.time()
+            print("1")
+    
+
+        
 
 if __name__ == "__main__":
     main()
